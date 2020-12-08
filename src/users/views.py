fb45354ca565384e402a11bb2c0de991bf27ac5c
Kamil <kamil.gorny99@gmail.com>
2020-12-08 16:50:14 +0100

    feat: Add login functionality

--- src/users/views.py
+++ src/users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect("rodeo-home")
+            messages.success(request, f'Your account has been created! You can log in')
+            return redirect("rodeo-login")
     else:
         form = UserRegisterForm()
     
