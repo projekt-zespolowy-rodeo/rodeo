2fd4cf6e12bbc83a72e0762f5a3790482446737d
Kamil <kamil.gorny99@gmail.com>
2020-12-08 13:26:37 +0100

    feat: Create cards for games

--- src/rodeo_app/models.py
+++ src/rodeo_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=50)
--- src/rodeo_app/views.py
+++ src/rodeo_app/views.py
@@ -4,11 +4,24 @@ games = [
     {
         'title': 'Kalambury',
         'description': 'Celem zabawy jest odgadywanie haseł. W każdej kolejce poszczególna drużyna odgaduje hasło lub hasła, przedstawiane przez jednego członka grupy. Z tego powodu często wymaga się, aby drużyna składała się z co najmniej 3 osób',
+        'genere': 'Gra Towarzyska'
     
     },
     {
         'title': 'Chińczyk',
-        'description': 'Gra dla 2-4 osób, w której celem graczy jest przejechanie dookoła planszy czterema pionkami z pozycji początkowych na końcowe. Pierwszy gracz, który tego dokona, wygrywa.'
+        'description': 'Gra dla 2-4 osób, w której celem graczy jest przejechanie dookoła planszy czterema pionkami z pozycji początkowych na końcowe. Pierwszy gracz, który tego dokona, wygrywa.',
+        'genere': 'Gra Towarzyska'
+    },
+      {
+        'title': 'Kalambury',
+        'description': 'Celem zabawy jest odgadywanie haseł. W każdej kolejce poszczególna drużyna odgaduje hasło lub hasła, przedstawiane przez jednego członka grupy. Z tego powodu często wymaga się, aby drużyna składała się z co najmniej 3 osób',
+        'genere': 'Gra Towarzyska'
+    
+    },
+    {
+        'title': 'Chińczyk',
+        'description': 'Gra dla 2-4 osób, w której celem graczy jest przejechanie dookoła planszy czterema pionkami z pozycji początkowych na końcowe. Pierwszy gracz, który tego dokona, wygrywa.',
+        'genere': 'Gra Towarzyska'
     }
 ]
 
