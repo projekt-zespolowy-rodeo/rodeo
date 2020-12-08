4caff2d4c5b7642affe88820c0219293a766db71
Kamil <kamil.gorny99@gmail.com>
2020-12-08 12:23:01 +0100

    feat: Add navbar

--- src/rodeo/settings.py
+++ src/rodeo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rodeo_app.apps.RodeoAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/rodeo_app/views.py
+++ src/rodeo_app/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
+games = [
+    {
+        'title': 'Kalambury',
+        'description': 'Celem zabawy jest odgadywanie haseł. W każdej kolejce poszczególna drużyna odgaduje hasło lub hasła, przedstawiane przez jednego członka grupy. Z tego powodu często wymaga się, aby drużyna składała się z co najmniej 3 osób',
+    
+    },
+    {
+        'title': 'Chińczyk',
+        'description': 'Gra dla 2-4 osób, w której celem graczy jest przejechanie dookoła planszy czterema pionkami z pozycji początkowych na końcowe. Pierwszy gracz, który tego dokona, wygrywa.'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'games': games
+    }
+    return render(request, 'rodeo_app/home.html', context)
+
+
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
-    
+    return render(request, 'rodeo_app/about.html')
