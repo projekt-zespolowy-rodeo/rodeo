9997078e12c706745b5afd94c6aff09e8261ef4b
Kamil <kamil.gorny99@gmail.com>
2020-12-08 20:03:48 +0100

    feat: Add images to games on home page

--- src/rodeo_app/models.py
+++ src/rodeo_app/models.py
@@ -5,6 +5,7 @@ class Game(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     genre = models.TextField()
+    image = models.ImageField(default='default.jpg', upload_to='images')
 
     def __str__(self):
         return self.title
