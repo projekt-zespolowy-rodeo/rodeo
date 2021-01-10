651b83c520e747e6ed5568c3353f1c04742ac319
Wojtek <wjachimowski@wp.pl>
2021-01-10 21:37:10 +0100

    Adding pictionary game written in Node JS

---
+++ Kalambury/client/src/client.js
@@ -0,0 +1,67 @@
+let can_paint = false;
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+const sock = io();
+let identification=0;
+let my_turn = false;
+const print = (text)=>{
+    const parent = document.querySelector('#wydarzenie');
+    const list_element = document.createElement('li');
+    list_element.innerHTML = text
+    parent.appendChild(list_element);
+
+};
+const after_submition_chat = (sock)=>(element) =>{
+element.preventDefault();
+const input = document.querySelector('#napis');
+sock.emit('chat_guess',[input.value,identification]);
+input.value = '';
+};
+const start_drawing = (element)=>{
+    can_paint =true;
+    send_drawing_coordinates(element);
+}
+const finish_drawing = (element)=>{
+    ctx.beginPath();
+    can_paint=false;
+}
+const set_id = (id)=>{
+    identification = id;
+}
+const set_chosen_id=(id_from_emit,word)=>{
+    console.log(id_from_emit);
+    if(identification == id_from_emit){
+        my_turn= true;
+        console.log(word);
+        print("Musisz narysowac: "+word);
+    }
+    else{
+        my_turn = false;
+    }
+}
+const send_drawing_coordinates =(element)=>{
+    if(!can_paint || !my_turn) return;
+    x=element.clientX-12;
+    y=element.clientY-20;
+    sock.emit('turn_coordinates',{x,y,x,y});
+
+}
+
+const draw_from = (x,y,w,z)=>{
+    ctx.lineCap="round";
+    ctx.lineWidth=5;
+    ctx.lineTo(x,y);
+    ctx.beginPath();
+    ctx.moveTo(w,z);
+}
+
+(()=>{
+    sock.on('chat_message',print);
+    sock.on('id',(id)=>set_id(id));
+    sock.on('turn_coordinates',({x,y,w,z})=>draw_from(x,y,w,z));
+    sock.on('whose_turn',([id_from_emit,chosen_word])=>set_chosen_id(id_from_emit,chosen_word));
+    canvas.addEventListener("mousedown",start_drawing);
+    canvas.addEventListener("mousemove",send_drawing_coordinates);
+    canvas.addEventListener("mouseup",finish_drawing);
+    document.querySelector('#form').addEventListener('submit',after_submition_chat(sock));
+})();
---
+++ Kalambury/server/server.js
@@ -0,0 +1,54 @@
+const express  = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const app = express();
+app.use(express.static(`${__dirname}/../client`));
+const server = http.createServer(app);
+const io = socketio(server);
+let list_of_id = [];
+let id_to_start = 0;
+let chosen_id = 0;
+let chosen_word = "";
+let is_more_than_2 =false;
+let array_with_words = ["pies","kot","krowa","czajnik","gitara"];
+function check_if_correct_word(chat_message,word,id){
+    console.log(chat_message+" - "+word);
+    if(chosen_id == id) return;
+    if(is_more_than_2){
+    if(chat_message == word) {
+        io.emit('chat_message',"Tak");
+    }
+    else{
+        io.emit('chat_message',chat_message);
+    }
+}
+else{
+    io.emit('chat_message',chat_message);
+}
+}
+io.on('connection',(sock)=>{
+    let id = 0
+    for(var i=0;i<list_of_id.length;i++){
+        if(list_of_id[i]==id){
+            id+=1;
+        }
+    }
+    list_of_id.push(id);
+    if(list_of_id.length>2){
+        sock.emit('chat_message', 'Too many players ... Disconnected');
+        sock.disconnect();
+    }
+    sock.emit('chat_message', 'Connection established with id: '+id);
+    sock.emit('id',id);
+    if(list_of_id.length==2){
+        chosen_word = array_with_words[Math.floor(Math.random()*array_with_words.length)]
+        chosen_id = list_of_id[id_to_start];
+        io.emit('whose_turn',[chosen_id,chosen_word]);
+        is_more_than_2=true;
+    }
+    sock.on('chat_guess',([text,id]) => check_if_correct_word(text,chosen_word,id))
+    sock.on('turn_coordinates',({x,y,w,z}) => io.emit('turn_coordinates',{x,y,w,z}));
+});
+server.listen(9999,()=>{
+    console.log("OK");
+})
