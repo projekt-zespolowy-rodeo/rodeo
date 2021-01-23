4acd805d5558b11e953c90ad68ab806df2c7d55e
Wojtek <wjachimowski@wp.pl>
2021-01-23 23:28:02 +0100

    Adding tic tac toe

---
+++ KolkoIKrzyzyk/client/src/client.js
@@ -0,0 +1,83 @@
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+const sock = io();
+let wX=Math.floor(500/3);
+let wY=Math.floor(500/3);
+var identyfication=-1;
+var colour ="";
+let point = 0;
+const click_on_the_box = (element)=>{
+    
+    x=element.clientX;
+    y=element.clientY;
+    sock.emit("coordinates_pointed",[x,y,identyfication,colour]);
+}
+const points= ()=>{
+    document.getElementById('score').innerHTML = "Points: "+point;
+}
+const click_on_the_box_print = (x,y,col)=>{
+    indexX=-1;
+    indexY=-1;
+    for(let i=0;i<3;i++){
+    if(0+wX*i<x && x<wX+wX*i){
+        indexX=i;
+    }
+    if(0+wY*i<y && y<wY+wY*i){
+        indexY=i;
+        }
+    }
+    const helper=(x,y)=>{
+    console.log(col);
+    ctx.fillStyle = col;
+    ctx.fillRect(x*wX+2,y*wY+2,wX-4,wY-4);
+    }
+    helper(indexX,indexY);
+}
+
+const draw_board =(width,heigth) => {
+    ctx.beginPath();
+    for(let i=0;i<4;i++){
+        ctx.lineWidth=5;
+        ctx.lineCap="round";
+        ctx.moveTo(wX*i,0);
+        ctx.lineTo(wX*i,heigth);
+        ctx.moveTo(0,wY*i);
+        ctx.lineTo(width,wY*i);
+    }
+    ctx.stroke();
+}
+const setId = (id,col) => {
+    identyfication = id;
+    colour = col;
+    console.log(colour);
+}
+
+(()=>{
+    sock.on('draw_board',(message)=>{
+        draw_board(500,500);
+        points();
+    });
+    sock.on("checked_value",([x,y,col])=>{click_on_the_box_print(x,y,col)});
+    sock.on('id',([id,colour])=>setId(id,colour));
+    canvas.addEventListener("mousedown",click_on_the_box);
+    sock.on('winning',(id)=>
+    {   
+        if(id == identyfication){
+        point ++;
+        points();
+        sock.emit('whoseTurn',"");
+        window.alert("You win !!!");
+        }
+        else if(id != -1){
+            window.alert("You lose !!!");
+        }
+        else{
+            sock.emit('whoseTurn',"Draw");
+            window.alert("It's a draw");
+        }
+        ctx.clearRect(0,0,500,500);
+        draw_board(500,500);
+        
+    }
+    )
+})();
---
+++ KolkoIKrzyzyk/server/server.js
@@ -0,0 +1,138 @@
+const express  = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const app = express();
+app.use(express.static(`${__dirname}/../client`));
+const server = http.createServer(app);
+const io = socketio(server);
+// -1 means this field is empty
+// 0 and 1 mean id of player
+let board =[[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+let list_of_id=[];
+let starting_whose_turn = 0;
+let whose_turn = 0;
+let wX=Math.floor(500/3);
+let wY=Math.floor(500/3);
+const checkWinning = () => {
+    for(var i = 0; i<3;i++){
+        if(board[i][0] != -1 && board[i][1] != -1 && board[i][2] != -1){
+        if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] == board[i][2]){
+            return board[i][0];
+        }}
+        if(board[0][i] != -1 && board[1][i] != -1 && board[2][i] != -1){
+        if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] == board[2][i]){
+            return board[0][i];
+        }
+        }
+    if(board[0][0] != -1 && board[1][1] != -1 && board[2][2] != -1){
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == board[2][2]){
+            return board[0][0];
+        }
+        }
+    if(board[0][2] != -1 && board[1][1] != -1 && board[2][0] != -1){
+        if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == board[2][0]){
+            return board[0][2];
+        }
+        }
+    }
+    return -1;
+}
+const ifCondition = () =>{
+    let value = -1;
+    let checkConditions = checkWinning();
+    if(checkConditions != -1){
+        value =checkConditions;
+        return value; //id of winner
+    }
+    let ifDraw = 0;
+    for(var i=0;i<3;i++){
+        for(var j=0;j<3;j++){
+            if(board[i][j] != -1){
+                ifDraw++;
+            }
+        }
+    }
+    if(ifDraw == 9){
+        return -1; //draw
+    }
+    else{
+        return -2; //not finished
+    }
+
+
+}
+
+const board_fuctionality = (x,y,id,colour) => {
+    var indexX = -1;
+    var indexY= -1;
+    for(let i=0;i<3;i++){
+        if(0+wX*i<x && x<wX+wX*i){
+            indexX=i;
+        }
+        if(0+wY*i<y && y<wY+wY*i){
+            indexY=i;
+            }
+    }
+    if (whose_turn == id){
+    if(board[indexX][indexY] == -1){
+    board[indexX][indexY] = id;
+    let checkConditions = ifCondition();
+    if(checkConditions == -2){
+    io.emit("checked_value",[x,y,colour]);
+    if(whose_turn == 0){
+        whose_turn = 1
+        }
+    else{
+        whose_turn = 0
+        }
+    
+}
+else if(checkConditions == -1){
+    io.emit("checked_value",[x,y,colour]);
+    io.emit("winning",checkConditions);
+}
+else{
+    io.emit("checked_value",[x,y,colour]);
+    io.emit("winning",checkConditions);
+}
+    }
+}
+}
+
+io.on('connection',(sock)=>{
+    let id = 0;
+    for(var i=0;i<list_of_id.length;i++){
+        if(list_of_id[i]==id){
+            id+=1;
+        }
+    }
+    list_of_id.push(id);
+    if(list_of_id.length>2){
+        sock.disconnect();
+    }
+    if(id == 0){
+        sock.emit('id',[id,"#A80006"]);
+    }
+    else{
+        sock.emit('id',[id,"#181818"]);  
+    }
+    sock.emit('draw_board',"");
+    sock.on("coordinates_pointed",([x,y,identification,colour])=>board_fuctionality(x,y,identification,colour)); 
+    sock.on('whoseTurn',(message)=>{
+        for(var i=0;i<3;i++){
+            for(var j=0;j<3;j++){
+                board[i][j] = -1;
+            }
+        }
+        if(starting_whose_turn == 0){
+            starting_whose_turn =1;
+            whose_turn =1;
+        }
+        else{
+            starting_whose_turn =0;
+            whose_turn = 0;
+        }});
+});
+server.listen(9998,()=>{
+    console.log("OK");
+})
